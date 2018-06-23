d965854f302d1661db9e1c1d8e7ab779b710deb6
longen0707 <279448059@qq.com>
2018-06-23 22:58:20 +0800

    添加字符串bbbb

--- itmayiedu-activemq/src/main/java/com/itmayiedu/activemq/TestGit.java
+++ itmayiedu-activemq/src/main/java/com/itmayiedu/activemq/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		String s="123456"; 
 		String a="aaaaaaaaaaa";
+		String b="bbbbbbbbbbb";
 		System.out.println("≤‚ ‘Git");
 	}
 }

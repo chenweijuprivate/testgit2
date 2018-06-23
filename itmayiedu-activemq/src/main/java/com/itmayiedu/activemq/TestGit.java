d13f90bd6799b3e5f23879f84c728451e067898d
longen0707 <279448059@qq.com>
2018-06-23 22:31:01 +0800

    添加字符串aaaaa

--- itmayiedu-activemq/src/main/java/com/itmayiedu/activemq/TestGit.java
+++ itmayiedu-activemq/src/main/java/com/itmayiedu/activemq/TestGit.java
@@ -3,6 +3,7 @@ package com.itmayiedu.activemq;
 public class TestGit {
 	public static void main(String[] args) {
 		String s="123456"; 
+		String a="aaaaaaaaaaa";
 		System.out.println("≤‚ ‘Git");
 	}
 }
